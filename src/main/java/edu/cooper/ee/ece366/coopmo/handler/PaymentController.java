f55bdfc5f2a7e15e64669e5407e61e5a39cc4f8a
Do Hyung Kwon <thedavekwon@gmail.com>
2020-02-23 22:55:49 -0500

    hashmaps added

--- src/main/java/edu/cooper/ee/ece366/coopmo/handler/PaymentController.java
+++ src/main/java/edu/cooper/ee/ece366/coopmo/handler/PaymentController.java
@@ -1,10 +1,27 @@
 package edu.cooper.ee.ece366.coopmo.handler;
 
+import edu.cooper.ee.ece366.coopmo.model.Payment;
+import edu.cooper.ee.ece366.coopmo.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 @RequestMapping("/pay")
 public class PaymentController {
+    private static ConcurrentHashMap<Long, Payment> paymentDB = new ConcurrentHashMap<>();
 
+    @GetMapping("/createPayment")
+    public Payment createPayment(
+            @RequestParam(value = "fromUserId", defaultValue = "") Long fromUserId,
+            @RequestParam(value = "toUserId", defaultValue = "") Long toUserId,
+            @RequestParam(value = "amount", defaultValue = "") Long amount,
+            @RequestParam(value = "type", defaultValue = "") Long type) {
+        Payment newPayment = new Payment((long) paymentDB.size(), fromUserId, toUserId, amount, type);
+        paymentDB.put(newPayment.id, newPayment);
+        return newPayment;
+    }
 }
--- src/main/java/edu/cooper/ee/ece366/coopmo/handler/UserController.java
+++ src/main/java/edu/cooper/ee/ece366/coopmo/handler/UserController.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private static ConcurrentHashMap<Long, User> userDB = new ConcurrentHashMap<>();
+
     // TODO(error handling if something is missing)
     // @PostMapping("/createUser")
     @GetMapping("/createUser")
@@ -20,9 +23,14 @@ public class UserController {
             @RequestParam(value = "password", defaultValue = "") String password,
             @RequestParam(value = "email", defaultValue = "") String email,
             @RequestParam(value = "handle", defaultValue = "") String handle) {
-        User newUser = new User(name, username, password, email, handle);
-
-//        userDB.put(newUser.id, newUser); //TODO(not working)
+        User newUser = new User((long) userDB.size(), name, username, password, email, handle);
+        userDB.put(newUser.id, newUser);
         return newUser;
+    }
+
+    // Debug Purpose
+    @GetMapping("/getUserSize")
+    public Integer getUserSize() {
+        return userDB.size();
     }
 }

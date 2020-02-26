6d79aadf36ad2cb97788a83f6d7b779273a74479
JampherS <shyam.paidipati@gmail.com>
2020-02-26 15:45:00 -0500

    ResponseEntity for all but User

--- src/main/java/edu/cooper/ee/ece366/coopmo/handler/PaymentController.java
+++ src/main/java/edu/cooper/ee/ece366/coopmo/handler/PaymentController.java
@@ -7,18 +7,37 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import static edu.cooper.ee.ece366.coopmo.CoopmoApplication.paymentDB;
+import static edu.cooper.ee.ece366.coopmo.CoopmoApplication.userDB;
 
 @RestController
 @RequestMapping("/pay")
 public class PaymentController {
     @GetMapping("/createPayment")
-    public Payment createPayment(
+    public ResponseEntity<String> createPayment(
             @RequestParam(value = "fromUserId", defaultValue = "") String fromUserId,
             @RequestParam(value = "toUserId", defaultValue = "") String toUserId,
             @RequestParam(value = "amount", defaultValue = "") Long amount,
             @RequestParam(value = "type", defaultValue = "") Integer type) {
+        if (!(userDB.containsKey(fromUserId))) {
+            return ResponseEntity.badRequest().body("User Id not valid");
+        }
+
+        if (!(userDB.containsKey(toUserId))) {
+            return ResponseEntity.badRequest().body("User Id not valid");
+        }
+
+        if ((userDB.get(fromUserId) == (userDB.get(toUserId)))) {
+            return ResponseEntity.badRequest().body("fromUserID and toUserID fields can not be the same");
+        }
+        if (amount <= 0) {
+            return ResponseEntity,badRequest().body("Amount can not be below 0 dollars");
+        }
+        if (type < 0 || type > 3) {
+            return ResponseEntity,badRequest().body("Type of transaction invalid");
+        }
+
         Payment newPayment = new Payment(fromUserId, toUserId, amount, type);
         paymentDB.put(newPayment.getId(), newPayment);
-        return newPayment;
+        return ResponseEntity.status(HttpStatus.OK).body("Your new Payment has been created");
     }
 }

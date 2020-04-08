455fb2db59903981a5e4cbd58435e2cab00a126d
Do Hyung Kwon <thedavekwon@gmail.com>
2020-04-08 01:54:31 -0400

    Added script to setup the webserver

--- create.sh
+++ create.sh
@@ -1,3 +1,30 @@
+docker network create coopmoNetwork
+
+set -e
+# https://stackoverflow.com/questions/25503412/how-do-i-know-when-my-docker-mysql-container-is-up-and-mysql-is-ready-for-taking
+function getContainerHealth {
+  docker inspect --format "{{.State.Health.Status}}" $1;
+}
+
+function waitContainer {
+  while STATUS=$(getContainerHealth $1); [ $STATUS != "healthy" ]; do
+    if [ $STATUS == "unhealthy" ]; then
+      echo "Failed!";
+      exit 1;
+    fi
+    printf .;
+    lf=$'\n';
+    sleep 1;
+  done
+  printf "$lf";
+}
+# setup database (pull from mysql docker image)
+docker run --name coopmoMysql --network coopmoNetwork --health-cmd='mysqladmin ping --silent' \
+           -e MYSQL_ROOT_PASSWORD=coopmopass -e MYSQL_DATABASE=coopmo_db -e MYSQL_USER=coopmo \
+           -e MYSQL_PASSWORD=coopmopass -p 3306:3306 -d mysql
+
+waitContainer coopmoMysql
+
+# build the container and run the app in 8080
 docker build -t thedavekwon/coopmo .
-docker run --name coopmo-mysql -e MYSQL_ROOT_PASSWORD=coopmopass -e MYSQL_DATABASE=coopmo_db -e MYSQL_USER=coopmo -e MYSQL_PASSWORD=coopmopass -p 3306:3306 -d mysql/mysql-server:latest
-docker run -p 8080:8080 -t thedavekwon/coopmo
+docker run --name coopmo --network coopmoNetwork -p 8080:8080 -t thedavekwon/coopmo
--- createdb.sh
+++
@@ -1,3 +0,0 @@
-create database coopmo_db;
-create user 'coopmo'@'%' identified by 'coopmopass';
-grant all on coopmo_db.* to 'coopmo'@'%';

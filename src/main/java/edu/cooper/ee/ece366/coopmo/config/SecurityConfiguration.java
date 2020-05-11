ac4ba46bb986897825953732ebe8ecfb7fd8b2d8
JampherS <shyam.paidipati@gmail.com>
2020-05-11 19:26:24 -0400

    Opened /authenticate to all requests

--- src/main/java/edu/cooper/ee/ece366/coopmo/config/SecurityConfiguration.java
+++ src/main/java/edu/cooper/ee/ece366/coopmo/config/SecurityConfiguration.java
@@ -64,7 +64,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/user/createUser");
+        web.ignoring().antMatchers("/user/createUser").antMatchers("/authenticate");
     }
 
     @Bean
